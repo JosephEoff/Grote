cb1cde959881322626253e04de5571ad2e482b11
Joseph Eoff <joseph.eoff@molear.de>
2023-02-18 22:21:26 +0100

    Removed bidirectional photo scan.

--- Scanners/Scanner_Photo.py
+++ Scanners/Scanner_Photo.py
@@ -14,19 +14,22 @@ class Scanner_Photo(ScannerBase):
             
         self.driver.parkScanner()
         self.SendScannerSettingsToDeviceIfNeeded()
+        y1 =self.driver.getYHome()
+        y2 = self.driver.getYRange()
+        step = 1
         for x in range(self.driver.getXHome(), self.driver.getXRange()):
             if not self.keepRunning:
                 self.driver.parkScanner()
                 return
             self.driver.moveX(x)
-            if x % 2 == 0:
-                y1 =self.driver.getYHome()
-                y2 = self.driver.getYRange()
-                step = 1
-            else:
-                y2 =self.driver.getYHome() -1
-                y1 = self.driver.getYRange() -1
-                step = -1
+#            if x % 2 == 0:
+#                y1 =self.driver.getYHome()
+#                y2 = self.driver.getYRange()
+#                step = 1
+#            else:
+#                y2 =self.driver.getYHome() -1
+#                y1 = self.driver.getYRange() -1
+#                step = -1
             for y in range(y1,  y2,  step): 
                 if not self.keepRunning:
                     self.driver.parkScanner()
